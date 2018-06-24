5adf6292d247071e403933e6aa9d842db40e1afd
Cliff Hall <cliff@futurescale.com>
2018-06-23 20:01:13 -0400

    Updated this old demo so that it doesn't appear to have been written by an animal.

--- src/socket-server.js
+++ src/socket-server.js
@@ -1,36 +1,28 @@
 // socket-server.js
 
 // Get config file specified on command line
-var config;
-var path = process.argv[2];
-var myid = process.argv[3];
-var usage = 'Usage: node socket-server.js path/to/servers.json server-id';
+let config;
+let path = process.argv[2];
+let myid = process.argv[3];
+let usage = 'Usage: node socket-server.js path/to/servers.json server-id';
 if (!path) {
     console.log(usage);
     process.exit();
 } else {
-    console.log('Loading config from: ' + path);
+    console.log(`Loading config from: ${path}`);
     try {
-        var fs = require('fs');
+        let fs = require('fs');
         config = JSON.parse(fs.readFileSync(path, 'utf8'));
     } catch (err) {
         console.log('Loading failed or config could not be parsed.');
         process.exit();
     }
     console.log('Config loaded.');
-    console.log('My id: ' + myid);
+    console.log(`My id: ${myid}`);
 }
 
 // Splice out the config for this server
-var myConfig;
-var i,server;
-for (i=0; i<config.servers.length; i++) {
-    server = config.servers[i];
-    if (server.id === myid) {
-        myConfig = config.servers.splice(i,1)[0];
-        break;
-    }
-}
+let myConfig = config.servers.find(server => server.id === myid);
 
 // Bail if we didn't find our config
 if (!myConfig){
@@ -46,15 +38,15 @@ const CONNECTION = 'connection';
 const DISCONNECT = 'disconnect';
 
 // Connection hashes
-var peers = {};
-var users = {};
-var usersByConnection = {};
+let peers = {};
+let users = {};
+let usersByConnection = {};
 
 // Listen for clients
-var port = myConfig.port;
-var socket = require('socket.io')(port);
+let port = myConfig.port;
+let socket = require('socket.io')(port);
 socket.on(CONNECTION, onConnection);
-console.log('Listening for connections on port:' + port);
+console.log(`Listening for connections on port: ${port}`);
 
 // Handle connection from clients (peers or users)
 function onConnection(connection) {
@@ -66,7 +58,7 @@ function onConnection(connection) {
 
     // Handle an identification event from a user
     function onIdentify(userId) {
-        var user = users[userId];
+        let user = users[userId];
         if (user){
             user.push(connection);
         } else {
@@ -78,55 +70,45 @@ function onConnection(connection) {
 
     // Handle an 'im' event from a client
     function onIm(message) {
-        var forwarded = message.forwarded?'forwarded':'';
-        console.log('Received '+ forwarded + ' IM from ' + message.from + ' to '+ message.to + ': ' + message.text);
-        var userConnections = users[message.to];
+        console.log(`Received ${message.forwarded?'forwarded ':''}IM from ${message.from} to ${message.to}: ${message.text}`);
+        let userConnections = users[message.to];
         if (userConnections) { // User is connected to this server
-            console.log('Recipient ' + message.to + ' has ' + userConnections.length + ' connection(s) to this server, sending...');
-            var userConnection, i;
-            for (i=0;i<userConnections.length;i++){
-                userConnection = userConnections[i];
-                userConnection.emit(IM, message);
-            }
+            console.log(`Recipient ${message.to} has ${userConnections.length} connection${userConnections.length>1?'s':''} to this server, sending...`);
+            userConnections.forEach(userConnection => userConnection.emit(IM, message));
         } else {
-            console.log('Recipient ' + message.to + ' not on this server');
+            console.log(`Recipient ${message.to} not connected to this server`);
         }
 
         // If message wasn't forwarded from another server, also forward to peers
         if (!message.forwarded){
             console.log('Forwarding to peers...');
             message.forwarded = true;
-            var peer, peerId, j;
-            for (j=0;j<config.servers.length;j++){
-                peerId = config.servers[j].id;
-                peer = peers[peerId];
+            config.servers.forEach( server => {
+                let peer = peers[server.id];
                 peer.emit(IM, message);
-            }
+            });
         } else {
             console.log('Message was forwarded, the buck stops here');
         }
-
     }
 
-    // Handle disconnection from a client
+    // Handle disconnect from a client
     function onDisconnect() {
-        // If it is a user, remove from users list
-        var userId = usersByConnection[connection.id];
+        // If it is a user, remove from users by connection list
+        let userId = usersByConnection[connection.id];
         if (userId) {
             delete usersByConnection[connection.id];
-            var userConnections = users[userId];
-            var userConnection;
+            let userConnections = users[userId];
             if (userConnections) {
-                console.log('User ' + userId + ' disconnected.');
-                for (var i=0;i<userConnections.length;i++) {
-                    userConnection = userConnections[i];
+                // Remove connection from the user's collection
+                console.log(`User ${userId} disconnected.`);
+                userConnections.forEach( (userConnection, index) => {
                     if (userConnection.id === connection.id){
-                        userConnections.splice(i,1);
-                        break;
+                        userConnections.splice(index, 1);
                     }
-                }
+                });
                 if (userConnections.length > 0) {
-                    console.log('User ' + userId + ' still has ' + userConnections.length + ' connections.');
+                    console.log(`User ${userId} still has ${userConnections.length} connections.`);
                 } else {
                     delete users[userId];
                 }
@@ -141,34 +123,31 @@ function onConnection(connection) {
 
     // Report user connections
     function reportUserConnections(user){
-        console.log('User: ' + user + ' connected ' + users[user].length + ' times.');
+        console.log(`User: ${user} connected ${users[user].length} times.`);
     }
 }
 
 // Initiate connection to peers
-var io = require('socket.io-client');
+let io = require('socket.io-client');
 connectToPeers(config.servers);
 
 // Connect to peer servers
 function connectToPeers(servers){
     console.log('Attempting to connect to peers...');
-    var host, peer, i;
-    for (i=0; i<servers.length; i++){
-        peer = servers[i];
+    servers.forEach( peer => {
         if (!peers[peer.id]) {
 
             // Build host endpoint for peer
-            host = 'http://' + peer.ip + ':' + peer.port;
+            let host = `http://${peer.ip}:${peer.port}`;
 
             // Attempt connection
-            console.log('Attempt connection to peer: '+peer.id + ' at: ' + host);
+            console.log(`Attempt connection to peer: ${peer.id} at: ${host}`);
             let peerSocket = io.connect(host, {reconnection:true} );
             peerSocket.peerId = peer.id;
 
             // Handle connection success
             peerSocket.on(CONNECT, function() {
-
-                console.log('Outbound connection to peer: ' + this.peerId);
+                console.log(`Outbound connection to peer: ${this.peerId}`);
 
                 // Store the peer connection
                 peers[this.peerId] = peerSocket;
@@ -179,10 +158,9 @@ function connectToPeers(servers){
 
             // Peer disconnected
             function onDisconnect(){
-                console.log('Peer: ' + this.peerId + ' disconnected. Will retry automatically.');
+                console.log(`Peer: ${this.peerId} disconnected. Will retry automatically.`);
             }
         }
-
-    }
+    });
 }
 
